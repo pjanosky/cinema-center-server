d5c5c347a2215f3f5337f315bf1103a2bfb6d9ee
pjanosky <pcjanosky@gmail.com>
2024-04-15 05:53:10 -0400

    fix cookie issues

--- src/App.ts
+++ src/App.ts
@@ -34,7 +34,6 @@ if (process.env.NODE_ENV !== "development") {
     sameSite: "none",
     secure: true,
     maxAge: 1000 * 60 * 60 * 24 * 365,
-    domain: process.env.ORIGIN_URL,
   };
 }
 app.use(session(sessionOptions));
